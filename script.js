8209b7cf77d29de0fcb941ac55ea6c7cd4ef9343
ali-alzaiat <alialzaiat2000@gmail.com>
2023-07-14 18:25:57 +0300

    Added comments

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function uptime() {
   min = d.getMinutes();
   sec = d.getSeconds();
   hr_rotation = 30 * hr + min / 2; //converting current time
-  min_rotation = 6 * min;
-  sec_rotation = 6 * sec;
+  min_rotation = 6 * min;          //converting minutes to degree
+  sec_rotation = 6 * sec;          //converting seconds to degree
 
-  hour.style.transform = `rotate(${hr_rotation}deg)`;
-  minute.style.transform = `rotate(${min_rotation}deg)`;
-  second.style.transform = `rotate(${sec_rotation}deg)`;
+  hour.style.transform = `rotate(${hr_rotation}deg)`;     //rotate the hour div
+  minute.style.transform = `rotate(${min_rotation}deg)`;  //rotate the minute div
+  second.style.transform = `rotate(${sec_rotation}deg)`;  //rotate the second div
 }
