17f4baa36f2dbc09e4de03fb9ae1c7bb36cdb558
ali-alzaiat <alialzaiat2000@gmail.com>
2023-07-14 18:14:54 +0300

    Added an analog clock

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 setInterval(uptime, 1000);
 function uptime() {
-  time.innerHTML = new Date();
+  d = new Date();
+  time.innerHTML = d;
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
+  hr_rotation = 30 * hr + min / 2; //converting current time
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
 }
